48c7539e3b799fabd5bf0a0bfc07a8a21a822ab0
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2022-12-22 23:56:26 +0530

    Pattern

---
+++ This is c code/Eleventh.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+for(i=1;i<=10;i++)
+    printf("%d ",i);//1 2 3 4 5 6 7 8 9 10 
+printf("\n");
+
+for(i=10;i>=1;i--)
+     printf("%d ",i);//10 9 8 7 6 5 4 3 2 1
+ printf("\n");
+
+for(i=1;i<=10;i=i*2)
+     printf("%d ",i);//1 2 4 8 10
+ printf("\n");
+ 
+for(i=1,j=2;i<=10,j<=10;i++,j=j+2)
+ printf("i=%d,j=%d \n",i,j);
+ 
+for(i=1,j=2;(i<=5)&&(j<=10);i++,j=j+2)
+ printf("i=%d,j=%d \n",i,j);
+ 
+ for(i=1,j=2;(i<=5)||(j<=20);i++,j=j+2)
+ printf("i=%d,j=%d \n",i,j);
+ 
+for(i=1;i!=10;i++)
+ printf("i=%d \n",i);
+return 0;
+}
