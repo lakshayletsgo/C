8c8ffdbb29f2e8b5448b55fa970d297995cc7f66
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2022-12-22 18:59:33 +0530

    This is change in variables

--- Third.c
+++ Third.c
@@ -6,6 +6,13 @@ int main(){
     printf("Value of a is %d\n",a);
     
     b = 20;//assignment
-    printf(" New Value of b is %d", b);
+    printf("New Value of b is %d\n", b);
+
+//address of variables
+
+    
+    int c=10,d=20;
+    printf("address of a is %p\n",&c);	//address of a
+    printf("address of b is %p",&d);	//%p is format specifier of printing addresses
     return 0;
 }
