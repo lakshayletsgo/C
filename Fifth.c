fa3c007cbe85e23981fd5a93173c9ea694f4d9bf
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2022-12-22 19:18:08 +0530

    THis is fifth commit of c language

---
+++ Fifth.c
@@ -0,0 +1,12 @@
+//Read 2 numbers from the user and print their sum
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+int main(){
+    int a,b ;
+    printf("Enter 2 numbers : ");
+    scanf("%d%d",&a,&b);
+    printf("the sum is : %d",a+b);
+    
+    return 0;
+}
