57b18b4c8eedcb2d1942ac34d1ff016e4e545ad7
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2022-12-22 19:19:29 +0530

    This is a change

--- Fifth.c
+++ Fifth.c
@@ -1,12 +1,19 @@
-//Read 2 numbers from the user and print their sum
+//Read 2 numbers from the user and use the arithematic operators
 #include<stdio.h>
 #include<limits.h>
 #include<float.h>
 int main(){
+    
     int a,b ;
     printf("Enter 2 numbers : ");
     scanf("%d%d",&a,&b);
-    printf("the sum is : %d",a+b);
+
+    printf("the sum is : %d\n",a+b);
+    printf("the diff is : %d\n",a-b);
+    printf("the multiplication is : %d\n",a*b);
+    printf("the division is : %d\n",a/b);
+    printf("the modulus is : %d\n",a%b);
     
+
     return 0;
 }
