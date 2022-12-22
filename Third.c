afa764ad140afead2024d4f7b30830b40bd708a2
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2022-12-22 18:56:08 +0530

    THis is third commit to display variables

---
+++ Third.c
@@ -0,0 +1,11 @@
+//Variables 
+#include<stdio.h>
+int main(){
+    int a,b; //declaration
+    a =10;//assignment
+    printf("Value of a is %d\n",a);
+    
+    b = 20;//assignment
+    printf(" New Value of b is %d", b);
+    return 0;
+}
