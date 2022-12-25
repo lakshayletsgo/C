36b784210bd76734b818de22b028971cccdf3004
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2022-12-25 12:27:17 +0530

    Change

--- This is c code/25.c
+++ This is c code/25.c
@@ -16,3 +16,7 @@ int fibonacci(int i){   //Declaration
         return 1;}
     return fibonacci(i-1) + fibonacci(i-2);//Recursive call
 }
+
+
+// Enter number of values you want to print: 23
+//  0       1       1       2       3       5       8       13      21      34      55      89      144     233     377         61      987     1597    2584    4181    6765    10946   17711   28657
